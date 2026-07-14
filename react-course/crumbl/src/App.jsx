15dc90963666409c9b866b6494f786f6ac4762a7
tuhamaina-blip <tuhamaina@gmail.com>
2026-07-14 09:45:31 +0300

    feat: set up App.jsx with router and context providers

--- react-course/crumbl/src/App.jsx
+++ react-course/crumbl/src/App.jsx
@@ -1,9 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import { SavedProvider } from './context/SavedContext';
+
 function App() {
   return (
-    <div>
-      <h1>Crumbl</h1>
-    </div>
-  )
+    <Router>
+      <AuthProvider>
+        <SavedProvider>
+          <div className="min-h-screen flex flex-col">
+            <h1>Crumbl</h1>
+          </div>
+        </SavedProvider>
+      </AuthProvider>
+    </Router>
+  );
 }
 
-export default App
+export default App;
